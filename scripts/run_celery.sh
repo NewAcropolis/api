04a633c8ab40bc309053b68eb56aa277b96d51fa
kenlt-uk <kenlt.uk@gmail.com>
2019-10-04 19:03:53 +0100

    Update celery to improve logging
    Will require an update to google-fluentd to pick up celery logs so that celery logs are available to stackdriver

--- scripts/run_celery.sh
+++ scripts/run_celery.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 set +x
 
+ENV=development
+
+if [ ! -z "$1" ]; then
+    ENV=$1
+fi
+
+if [ "$ENV" = 'preview' ]; then
+    PORT=4555
+else
+    PORT=5555
+fi
+
 if [ -z "$VIRTUAL_ENV" ] && [ -d venv ]; then
   echo 'activate venv for celery'
   source ./venv/bin/activate
   nooutput=' >&- 2>&- <&- &'
 fi
 
-# kill existing celery workers
-ps auxww | grep 'celery worker' | awk '{print $2}' | xargs kill -9
+pip install flower==0.9.3
 
-eval "celery -A run_celery.celery worker --loglevel=INFO --concurrency=1"$nooutput
+# kill existing celery workers
+ps auxww | grep "celery worker-$ENV" | awk '{print $2}' | xargs kill -9
+
+# kill flower
+lsof -i :$PORT  | awk '{if(NR>1)print $2}' | xargs kill -9
+
+FLOWER_BASIC_AUTH=admin:test
+
+eval "celery -A run_celery.celery worker --loglevel=INFO -n worker-$ENV@%h --concurrency=1"$nooutput
+eval "celery -A run_celery.celery flower --url_prefix=celery --address=127.0.0.1 --port=$PORT"$nooutput
