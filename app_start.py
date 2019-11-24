8304a6c679e643aa87a5b1183ec2f57e4728ef15
kenlt-uk <kenlt.uk@gmail.com>
2019-11-24 00:10:44 +0000

    Add command to import magazine

--- app_start.py
+++ app_start.py
@@ -1,4 +1,8 @@
+import base64
+import json
 import os
+import re
+import requests
 from flask_script import Manager, Server
 from app import create_app, db
 from app.storage.utils import Storage
@@ -26,6 +30,72 @@ def generate_web_images(year=None):
     application.logger.info('Generate web images')
     storage = Storage(application.config['STORAGE'])
     storage.generate_web_images(year)
+
+
+@manager.command
+def upload_magazines(folder):
+    """Upload magazines."""
+    application.logger.info('Upload magazines')
+    storage = Storage(application.config['STORAGE'])
+
+    magazine_pattern = r'( |_)issue( |_)(?P<issue_no>\d+)( .*)?\.pdf'
+
+    share_items = []
+    with open(os.path.join('data', 'shareitems.json')) as f:
+        json_shareitems = json.loads(f.read())
+        for item in json_shareitems:
+            share_items.append(item)
+
+    access_token = get_access_token()
+
+    for item in share_items:
+        filename = item['ImageFilename']
+        match = re.search(magazine_pattern, filename, re.IGNORECASE)
+        if match:
+            issue_no = match.group('issue_no')
+
+            with open(os.path.join(folder, filename)) as f:
+                binary = f.read()
+                new_filename = 'bi_monthly_issue_{}.pdf'.format(issue_no)
+
+                storage.upload_blob_from_base64string(
+                    filename,
+                    new_filename,
+                    base64.b64encode(binary),
+                    content_type='application/pdf'
+                )
+
+                payload = {
+                    'old_id': item['id'],
+                    'title': item['Title'],
+                    'old_filename': item['ImageFilename'],
+                    'filename': new_filename
+                }
+
+                auth_request('magazine/import', access_token, payload)
+
+
+def get_access_token():
+    auth_payload = {
+        "username": application.config['ADMIN_CLIENT_ID'],
+        "password": application.config['ADMIN_CLIENT_SECRET'],
+    }
+
+    auth_response = requests.post(
+        os.path.join(application.config['API_BASE_URL'], 'auth/login'),
+        data=json.dumps(auth_payload),
+        headers={'Content-Type': 'application/json'},
+    )
+
+    return auth_response.json()["access_token"]
+
+
+def auth_request(endpoint, access_token, payload):
+    return requests.post(
+        os.path.join(application.config['API_BASE_URL'], endpoint),
+        data=json.dumps(payload),
+        headers={'Authorization': 'Bearer {}'.format(access_token)},
+    )
 
 
 if __name__ == '__main__':
