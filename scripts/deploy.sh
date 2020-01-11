9df8ddb78c9b391760b7eaad0dcde824361f9498
kenlt-uk <kenlt.uk@gmail.com>
2020-01-11 23:01:46 +0000

    Set venv before running celery

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -110,7 +110,7 @@ sudo systemctl restart na-api.service
 
 cd www-live
 set -a
-. ./na-api.env && ./scripts/run_celery.sh live $celery_output_params
+. ./venv/bin/activate && . ./na-api.env && ./scripts/run_celery.sh live $celery_output_params
 set +a
         """
     else
