55b8ec35e2ccebaeee2ed55c57680c0682a36928
kenlt-uk <kenlt.uk@gmail.com>
2020-03-07 02:05:14 +0000

    Add legacy route for event

--- tests/app/routes/legacy/test_rest.py
+++ tests/app/routes/legacy/test_rest.py
@@ -26,3 +26,36 @@ class WhenGettingLegacyImages:
 
         assert response.status_code == 200
         assert mock_get_blob.call_args[0][0] == 'thumbnail/2019/test.jpg'
+
+
+class WhenGettingLegacyEvent:
+    def it_gets_event_with_old_id(self, client, db_session, sample_event):
+        response = client.get(
+            url_for('legacy.event_handler', eventid=sample_event.old_id)
+        )
+
+        assert response.json['id'] == str(sample_event.id)
+
+    def it_raises_a_404_if_not_found(self, client):
+        response = client.get(
+            url_for('legacy.event_handler', eventid='1')
+        )
+
+        assert response.status_code == 404
+        assert response.json['message'] == 'event not found for old_id: 1'
+
+    def it_raises_a_400_if_id_not_int(self, client):
+        response = client.get(
+            url_for('legacy.event_handler', eventid='abc')
+        )
+
+        assert response.status_code == 400
+        assert response.json['message'] == 'invalid event old_id: abc'
+
+    def it_raises_a_400_if_no_id(self, client):
+        response = client.get(
+            url_for('legacy.event_handler')
+        )
+
+        assert response.status_code == 400
+        assert response.json['message'] == 'invalid event old_id: None'
