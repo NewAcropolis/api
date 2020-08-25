86154f4fa9e558f60e73dc2051311167755b0d23
kenlt-uk <kenlt.uk@gmail.com>
2020-08-25 23:58:23 +0100

    Restrict batch emails to civilised hours

--- app/config.py
+++ app/config.py
@@ -79,6 +79,8 @@ class Config(object):
     EMAIL_DELAY = 4 if ENVIRONMENT != 'development' else 0  # hours
     EMAIL_LIMIT = 400
     EMAIL_RESTRICT = os.environ.get('EMAIL_RESTRICT')
+    EMAIL_EARLIEST_TIME = "08:00:00"
+    EMAIL_LATEST_TIME = "22:00:00"
 
 
 class Development(Config):
