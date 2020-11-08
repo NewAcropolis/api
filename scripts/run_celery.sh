f86dc37053c8270a0c53675f605ed3728f567f07
kenlt-uk <kenlt.uk@gmail.com>
2020-11-08 00:33:23 +0000

    Uninstall celery flower

--- scripts/run_celery.sh
+++ scripts/run_celery.sh
@@ -7,11 +7,11 @@ if [ ! -z "$1" ]; then
     ENV=$1
 fi
 
-if [ "$ENV" = 'preview' ]; then
-    FLOWER_PORT=4555
-else
-    FLOWER_PORT=5555
-fi
+# if [ "$ENV" = 'preview' ]; then
+#     FLOWER_PORT=4555
+# else
+#     FLOWER_PORT=5555
+# fi
 
 if [ -z "$VIRTUAL_ENV" ] && [ -d env ]; then
   echo 'activate env for celery'
@@ -20,7 +20,8 @@ fi
 
 logoutput=" >>/var/log/na-api/celery-$ENV.log 2>&1 &"
 
-pip install flower==0.9.3
+# remove celery flower
+pip uninstall flower
 
 # kill existing celery workers
 ps auxww | grep "celery worker -n worker-$ENV" | awk '{print $2}' | xargs kill -9
@@ -36,14 +37,14 @@ eval "celery -A run_celery.celery worker -n worker-$ENV --loglevel=INFO --concur
 eval "celery -A run_celery.celery beat"$logoutput
 
 # kill celery flower
-FLOWER_PID=`lsof -i :$FLOWER_PORT  | awk '{if(NR>1)print $2}'`
+# FLOWER_PID=`lsof -i :$FLOWER_PORT  | awk '{if(NR>1)print $2}'`
 
-if [ -z "$FLOWER_PID" -o "$RESTART_FLOWER" ]; then
-  if [ ! -z "$FLOWER_PID" ]; then
-    kill -9 $FLOWER_PID
-  fi
-  # eval "celery -A run_celery.celery flower --url_prefix=celery --address=0.0.0.0 --port=$FLOWER_PORT"$logoutput
-fi
+# if [ -z "$FLOWER_PID" -o "$RESTART_FLOWER" ]; then
+#   if [ ! -z "$FLOWER_PID" ]; then
+#     kill -9 $FLOWER_PID
+#   fi
+#   # eval "celery -A run_celery.celery flower --url_prefix=celery --address=0.0.0.0 --port=$FLOWER_PORT"$logoutput
+# fi
 
 # check that celery has started properly
 num_workers=$(( $(ps auxww | grep "celery worker -n worker-$ENV" | wc -l) -1))
