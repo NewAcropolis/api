f6ba8a566160639ddaf9276c7e09dbea7ac4ded4
kenlt-uk <kenlt.uk@gmail.com>
2021-05-17 02:02:27 +0100

    Create paypal button if in right state
    test during event creation.

---
+++ app/routes/orders/schemas.py
@@ -0,0 +1,13 @@
+post_update_order_address_schema = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "description": "POST schema for update order address",
+    "type": "object",
+    "properties": {
+        "address_street": {"type": "string"},
+        "address_city": {"type": "string"},
+        "address_state": {"type": "string"},
+        "address_postal_code": {"type": "string"},
+        "address_country_code": {"type": "string"},
+    },
+    "required": ["address_street", "address_city", "address_postal_code", "address_country_code"]
+}
