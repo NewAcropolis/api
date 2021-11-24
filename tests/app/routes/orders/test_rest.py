f77e4a4d1160b5d8a959b6b38c533d48bccf9498
kenlt-uk <kenlt.uk@gmail.com>
2021-11-24 00:46:16 +0000

    Update get orders for invalid orders and set original TXN_ID after IPN replay

--- tests/app/routes/orders/test_rest.py
+++ tests/app/routes/orders/test_rest.py
@@ -418,14 +418,14 @@ class WhenHandlingPaypalIPN:
             assert 'http://test/images/qr_codes/{}'.format(
                 str(tickets[n].id)) in mock_send_email.call_args_list[i][0][2]
 
-    def it_replaces_existing_orders_and_event_tickets_on_replay_using_txn_id(
+    def it_replaces_existing_invalid_order_and_event_tickets_on_replay_using_txn_id(
         self, mocker, client, db_session, sample_event_with_dates
     ):
         mocker.patch('app.routes.orders.rest.Storage')
         mocker.patch('app.routes.orders.rest.Storage.upload_blob_from_base64string')
         mock_send_email = mocker.patch('app.routes.orders.rest.send_email')
 
-        txn_id = '112233'
+        txn_id = 'XX-INVALID_1637667646-112233'
         txn_type = 'cart'
 
         _sample_ipn = sample_ipns[0].format(
@@ -433,7 +433,7 @@ class WhenHandlingPaypalIPN:
         _params = json.dumps(parse_qs(_sample_ipn))
 
         order = create_order(
-            txn_id='112233', created_at='2018-01-01 10:00:00', params=_params
+            txn_id=txn_id, created_at='2018-01-01 10:00:00', params=_params
         )  # original order
         assert len(order.tickets) == 0
 
@@ -442,7 +442,6 @@ class WhenHandlingPaypalIPN:
 
             client.post(
                 url_for('orders.replay_paypal_ipn', txn_id=txn_id),
-                # data=_sample_ipn,
                 content_type="application/x-www-form-urlencoded",
                 headers=[
                     ('Content-Type', 'application/json'),
@@ -456,7 +455,7 @@ class WhenHandlingPaypalIPN:
 
         assert mock_send_email.called
         assert len(orders) == 1
-        assert orders[0].txn_id == txn_id
+        assert orders[0].txn_id == txn_id[22:]
         assert orders[0].txn_type == txn_type
 
         tickets = dao_get_tickets_for_order(orders[0].id)
@@ -1320,6 +1319,18 @@ class WhenGettingOrders:
         assert len(response.json[0]['linked_transactions']) == 2
         assert response.json[0]['payment_total'] == "10.00"
         assert response.json[0]['delivery_balance'] == "0.0"
+
+    def it_will_return_latest_invalid_orders(self, client, db_session, sample_order):
+        order = create_order(
+            txn_id='XX-INVALID_1637667646-2223334455',
+            payment_total=1
+        )
+        response = client.get(
+            url_for('orders.get_orders', _filter='invalid')
+        )
+
+        assert len(response.json) == 1
+        assert response.json[0]['txn_id'] == order.txn_id
 
     def it_will_return_ignore_duplicate_order(self, client, db_session, sample_order):
         create_order(
