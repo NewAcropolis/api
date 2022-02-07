5890a23fe5b5ef3591cd9d58812882ee3f3d8c05
kenlt-uk <kenlt.uk@gmail.com>
2022-02-07 01:22:37 +0000

    Update run_local_celery script

--- scripts/run_local_celery.sh
+++ scripts/run_local_celery.sh
@@ -1,2 +1,5 @@
+ps auxww | grep "run_celery.celery worker" | awk '{print $2}' | xargs kill -9
+ps auxww | grep "run_celery.celery beat" | awk '{print $2}' | xargs kill -9
+rm celerybeat.pid
 watchmedo auto-restart --directory=./ --pattern=*.py --recursive -- celery -A run_celery.celery beat &
 watchmedo auto-restart --directory=./ --pattern=*.py --recursive -- celery -A run_celery.celery worker -n worker-development --loglevel=INFO --concurrency=1
